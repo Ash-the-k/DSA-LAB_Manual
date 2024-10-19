f21a1bb501a0dc54f495fc988d07a75fdbfca1a2
Ashlesh <ashlesh.kanchan@gmail.com>
2024-10-19 20:10:31 +0530

    cq

--- Exp4/Exp6_cq.c
+++ Exp4/Exp6_cq.c
@@ -10,105 +10,89 @@ Display the status of the Circular Queue.
 Exit the program.
 */
 
-#include<stdio.h>
-#include<stdlib.h>
 
-#define MAX 10
+#include <stdio.h>
+#define MAX 5
 
-int cq[MAX];
+int q[MAX], front = -1, rear = -1;
 
-int front, rear;
-
-void initialize();
-int isEmpty();
+void cenqueue();
+void cdequeue();
+void disp();
 int isFull();
-void eq();
-void dq();
-void display();
+int isEmpty();
 
-int main(){
+void main() {
     int ch;
-    printf("Enter your Choice : ");
-    while(1){
+    do {
         printf("\n**** MENU ****");
         printf("\n1. Insert\n2. Delete\n3. Display\n4. Exit");
         printf("\nEnter your choice: ");
         scanf("%d", &ch);
 
-        switch (ch)
-        {
-        case 1:
-            eq();
-            break;
-        case 2:
-            dq();
-            break;
-        case 3:
-            display();
-            break;
-        
-        default:
-            printf("Enter correct option");
-            break;
+        switch(ch) {
+            case 1: cenqueue(); break;
+            case 2: cdequeue(); break;
+            case 3: disp(); break;
+            case 4: printf("\nExiting...\n"); break;
+            default: printf("\nInvalid choice");
         }
-        if(ch==4)   break;
-    }
+    } while(ch != 4);
 }
 
-void initialize(){
-    front = rear = -1;
+int isFull() {
+    return (rear + 1) % MAX == front;
 }
 
-int isEmpty(){
-    return (front == -1);
+int isEmpty() {
+    return front == -1;
 }
 
-int isFull(){
-    return ((rear+1)%MAX)==front;
-}
-
-void eq(){
-    if(isFull()){
-        printf("Overflow!!");
-        return;
-    }
-    printf("Enter item to eq");
+void cenqueue() {
     int item;
-    scanf("%d",&item);
-    if(front == -1 )    front = 0;
-    rear=(rear+1)%MAX;
-    cq[rear] = item;
-    printf("\n%d is Enqueued\n",item);
+    
+    if(isFull()) {
+        printf("\nQueue Overflow");
+    } else {
+        printf("\nEnter the number to insert: ");
+        scanf("%d", &item);
+
+        if(front == -1) {
+            front = 0;
+        }
+        rear = (rear + 1) % MAX;
+        q[rear] = item;
+        printf("\n%d is inserted in the queue", item);
+    }
 }
 
-void dq(){
-    if(isEmpty())
-    {
-        printf("\nUnderflow!!\n");
-        return;
-    }
+void cdequeue() {
     int item;
-    item = cq[front];
-    if(front == rear)
-    {
-        front = rear = -1;
+
+    if(isEmpty()) {
+        printf("\nQueue Underflow");
+    } else {
+        item = q[front];
+        printf("\n%d is deleted", item);
+
+        if(front == rear) {
+            front = -1;
+            rear = -1;
+        } else {
+            front = (front + 1) % MAX;
+        }
     }
-    else
-    front = (front+1)%MAX;
-    printf("\n%d is delted\n",item);
 }
 
-void display()
-{
-    if(isEmpty())
-    {
-        printf("\nUnderflow!!\n");
-        return;
+void disp() {
+    int i;
+
+    if(isEmpty()) {
+        printf("\nQueue is empty");
+    } else {
+        printf("\nQueue contains:\n");
+        for(i = front; i <= rear; i = (i + 1) % MAX) {
+            printf("%d\t", q[i]);
+        }
     }
-    int i = front;
-    while(i != rear){  // Traverse the queue circularly
-        printf("%d \t", cq[i]);
-        i = (i + 1) % MAX;  // Move to the next element circularly
-    }
-    printf("%d \t", cq[rear]);  
 }
